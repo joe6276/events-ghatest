b5c6aabf172dd7417150c32a61f68fa40c4556af
Jonathan Ndambuki <joendambuki16@gmail.com>
2025-04-09 10:51:29 +0300

    Deployment

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,19 +1,28 @@
 
-const core = require('@actions/core')
+const core = require('@actions/core') // input and output 
+const exec= require('@actions/exec') // uloading to S3
 
-const exec= require("@actions/exec")
+
 
 function run(){
+// Get inputs
 
-    const bucket = core.getInput('bucket', {required: true});
-    const bucketRegion = core.getInput('bucket-region', {required: true});
-    const distFolder = core.getInput('dist-folder', {required: true});
+const bucket =core.getInput('bucket', {required:true});
+const bucketRegion =core.getInput('bucket-region', {required:true});
+const distFolder =core.getInput('dist-folder', {required:true});
+//Upload files to S3
 
-    const s3Uri = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+const s3URI= `s3://${bucket}`
+exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
+// `aws s3 sync folder1 s3://dktcohort --region us-east-1`
 
-    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-    core.setOutput('website-url', websiteUrl);
+
+
+//Get URl
+const websiteUrl= `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+// http://dktcohort.s3-website-us-east-1.amazonaws.com
+core.setOutput('website-url',websiteUrl)
+
 }
 
 run()
